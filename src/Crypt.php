fcd55230186ef674e326214a2fde7a19a8a84b98
Enrico Zimuel <e.zimuel@gmail.com>
2016-03-06 07:40:53 -0600

    Added the options support and extended the unit tests

--- src/Crypt.php
+++ src/Crypt.php
@@ -1,43 +1,196 @@
 <?php
+/**
+ * Crypto class to encrypt/decrypt string using encrypt-then-authenticate
+ * technique with HMAC
+ *
+ * @author Enrico Zimuel (enrico@zimuel.it)
+ */
+declare(strict_types=1);
 
 namespace PHPCrypto;
 
 class Crypt
 {
+    /**
+     * Minimum number of PBKDF2 iteration allowed for security reason
+     * @see http://goo.gl/OzdRxi
+     */
+    const MIN_PBKDF2_ITERATIONS = 20000;
+
+    /**
+     * Minimum size of key in bytes
+     * @see https://en.wikipedia.org/wiki/Password_strength
+     */
+    const MIN_SIZE_KEY = 12;
+
+    /**
+     * @var string
+     */
     protected $algo = 'aes-256-cbc';
 
-    protected $iteration = 80000;
+    /**
+     * Set the default number of iteration 4x the min value
+     * @see https://goo.gl/bzv4dK
+     * @var int
+     */
+    protected $iterations = self::MIN_PBKDF2_ITERATIONS * 4;
 
-    protected $keySize = 32;
+    /**
+     * @var string
+     */
+    protected $hash = 'sha256';
 
-    protected $hashAlgo = 'sha256';
-
+    /**
+     * @var string
+     */
     protected $key;
 
+    /**
+     * @var int
+     */
+    private $hmacSize;
+
+    /**
+     * @var int
+     */
+    private $keySize = 32;
+
+    /**
+     * Constructor
+     */
     public function __construct(array $config = [])
     {
-        $this->calcHmacSize($this->hashAlgo);
+        if (!empty($config)) {
+          if (isset($config['algo'])) {
+              $this->setAlgorithm($config['algo']);
+          }
+          if (isset($config['hash'])) {
+              $this->setHash($config['hash']);
+          }
+          if (isset($config['iterations'])) {
+              $this->setIterations($config['iterations']);
+          }
+          if (isset($config['key'])) {
+              $this->setKey($config['key']);
+          }
+        }
+        $this->hmacSize = $this->getHmacSize($this->hash);
     }
 
-    protected function calcHmacSize($hashAlgo)
+    /**
+     * Calc the output size of hash_hmac
+     * @return int
+     */
+    protected function getHmacSize($hash) : int
     {
-        $this->hmacSize = strlen(hash_hmac($hashAlgo, openssl_random_pseudo_bytes(32), $this->key, true));
+        return strlen(hash_hmac($hash, 'test', openssl_random_pseudo_bytes(32), true));
     }
 
+    /**
+     * Set the encryption key
+     * @param string $key
+     */
     public function setKey(string $key)
     {
+        if (strlen($key) < self::MIN_SIZE_KEY) {
+            trigger_error(
+                sprintf("The encryption key %s it's too short!", $key),
+                E_USER_WARNING
+            );
+        }
         $this->key = $key;
     }
 
-    public function getKey() : string
+    /**
+     * Get the encryption key
+     * @return string
+     */
+    public function getKey(): string
     {
         return $this->key;
     }
 
     /**
-     * Encrypt-then-authenticate
+     * Set the symmetric encryption algorithm
+     * @param string $algo
+     * @throws \InvalidArgumentException
      */
-    public function encrypt(string $plaintext): string
+    public function setAlgorithm(string $algo)
+    {
+        if (!in_array($algo, openssl_get_cipher_methods(true))) {
+            throw new \InvalidArgumentException(sprintf(
+                "The algorithm %s is not supported by OpenSSL", $algo
+            ));
+        }
+        $this->algo = $algo;
+    }
+
+    /**
+     * Get the symmetric encryption algorithm
+     * @return string
+     */
+    public function getAlgorithm() : string
+    {
+        return $this->algo;
+    }
+
+    /**
+     * Set the hash algorithm for PBKDF2 and HMAC
+     * @param string $hash
+     * @throws \InvalidArgumentException
+     */
+    public function setHash(string $hash)
+    {
+        if (!in_array($hash, hash_algos())) {
+            throw new \InvalidArgumentException(sprintf(
+                "The hash algorithm %s is not supported", $hash
+            ));
+        }
+        $this->hash     = $hash;
+        $this->hmacSize = $this->getHmacSize($this->hash);
+    }
+
+    /**
+     * Get the hash algorithm used by PBKDF2 and HMAC
+     * @return string
+     */
+    public function getHash() : string
+    {
+        return $this->hash;
+    }
+
+    /**
+     * Set the number of iteration for PBKDF2
+     * @param int $iteration
+     */
+    public function setIterations(int $iterations)
+    {
+        // Security warning
+        if ($iterations < self::MIN_PBKDF2_ITERATIONS) {
+            trigger_error(
+                sprintf("The number of iteration %s used for PBKDF2 it's too low!", $iterations),
+                E_USER_WARNING
+            );
+        }
+        $this->iterations = $iterations;
+    }
+
+    /**
+     * Get the number of iterations for PBKDF2
+     * @return int
+     */
+    public function getIterations() : int
+    {
+        return $this->iterations;
+    }
+
+    /**
+     * Encrypt-then-authenticate with HMAC
+     * @param string $plaintext
+     * @return string
+     * @throws \RuntimeException
+     */
+    public function encrypt(string $plaintext) : string
     {
         if (empty($this->key)) {
             throw new \RuntimeException('The encryption key cannot be empty');
@@ -46,10 +199,11 @@ class Crypt
         $iv     = random_bytes($ivSize);
 
         // Generate an encryption and authentication key
-        $keys    = hash_pbkdf2($this->hashAlgo, $this->key, $iv, $this->iteration, $this->keySize * 2, true);
+        $keys    = hash_pbkdf2($this->hash, $this->key, $iv, $this->iterations, $this->keySize * 2, true);
         $encKey  = substr($keys, 0, $this->keySize); // encryption key
         $hmacKey = substr($keys, $this->keySize);    // authentication key
 
+        // Encrypt
         $ciphertext = openssl_encrypt(
             $plaintext,
             $this->algo,
@@ -57,13 +211,18 @@ class Crypt
             OPENSSL_RAW_DATA,
             $iv
         );
-
-        // authentication
-        $hmac = hash_hmac($this->hashAlgo, $iv . $ciphertext, $hmacKey, true);
+        // Authentication
+        $hmac = hash_hmac($this->hash, $iv . $ciphertext, $hmacKey, true);
         return $hmac . $iv . $ciphertext;
     }
 
-    public function decrypt(string $ciphertext): string
+    /**
+     * Authenticate-then-decrypt with HMAC
+     * @param string $ciphertext
+     * @return string
+     * @throws \RuntimeException
+     */
+    public function decrypt(string $ciphertext) : string
     {
         if (empty($this->key)) {
             throw new \RuntimeException('The decryption key cannot be empty');
@@ -74,13 +233,16 @@ class Crypt
         $ciphertext = substr($ciphertext, $ivSize + $this->hmacSize);
 
         // Generate the encryption and hmac keys
-        $keys    = hash_pbkdf2($this->hashAlgo, $this->key, $iv, $this->iteration, $this->keySize * 2, true);
+        $keys    = hash_pbkdf2($this->hash, $this->key, $iv, $this->iterations, $this->keySize * 2, true);
         $encKey  = substr($keys, 0, $this->keySize); // encryption key
         $hmacKey = substr($keys, $this->keySize);    // authentication key
-        $hmacNew = hash_hmac($this->hashAlgo, $iv . $ciphertext, $hmacKey, true);
+
+        // Authentication
+        $hmacNew = hash_hmac($this->hash, $iv . $ciphertext, $hmacKey, true);
         if (!hash_equals($hmac, $hmacNew)) {
 	         throw new \RuntimeException('Authentication failed');
         }
+        // Decrypt
         return openssl_decrypt(
             $ciphertext,
             $this->algo,
--- test/CryptTest.php
+++ test/CryptTest.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace PHPCryptoTest;
 
 use PHPCrypto\Crypt;
@@ -8,17 +10,98 @@ class CryptTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->crypt = new Crypt();
+        $this->crypt->setIterations(Crypt::MIN_PBKDF2_ITERATIONS);
+    }
+
+    public function testConstructor()
+    {
+        $crypt = new Crypt();
+        $this->assertInstanceOf(Crypt::class, $crypt);
+    }
+
+    public function testConstructWithOptions()
+    {
+        $algos = openssl_get_cipher_methods(true);
+        $algo = $algos[array_rand($algos)];
+        $hash = hash_algos()[array_rand(hash_algos())];
+        $iterations = Crypt::MIN_PBKDF2_ITERATIONS * 3;
+
+        $options = [
+            'algo'       => $algo,
+            'hash'       => $hash,
+            'iterations' => $iterations
+        ];
+        $crypt = new Crypt($options);
+        $this->assertInstanceOf(Crypt::class, $crypt);
+        $this->assertEquals($algo, $crypt->getAlgorithm());
+        $this->assertEquals($hash, $crypt->getHash());
+        $this->assertEquals($iterations, $crypt->getIterations());
     }
 
     public function testSetKey()
     {
+        $key = random_bytes(Crypt::MIN_SIZE_KEY);
+        $this->crypt->setKey($key);
+        $this->assertEquals($key, $this->crypt->getKey());
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testSetShortKey()
+    {
         $this->crypt->setKey('test');
-        $this->assertEquals('test', $this->crypt->getKey());
+    }
+
+    public function testSetIterations()
+    {
+        $iterations = Crypt::MIN_PBKDF2_ITERATIONS * 2;
+        $this->crypt->setIterations($iterations);
+        $this->assertEquals($iterations, $this->crypt->getIterations());
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testSetLowNumberOfIterations()
+    {
+        $this->crypt->setIterations(1);
+    }
+
+    public function testSetAlgorithm()
+    {
+        $algos = openssl_get_cipher_methods(true);
+        $algo = $algos[array_rand($algos)];
+        $this->crypt->setAlgorithm($algo);
+        $this->assertEquals($algo, $this->crypt->getAlgorithm());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetUndefinedAlgorithm()
+    {
+        $this->crypt->setAlgorithm('foo');
+    }
+
+    public function testSetHash()
+    {
+        $hash = hash_algos()[array_rand(hash_algos())];
+        $this->crypt->setHash($hash);
+        $this->assertEquals($hash, $this->crypt->getHash());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetUndefinedHash()
+    {
+        $this->crypt->setHash('foo');
     }
 
     public function testEncryptDecrypt()
     {
-        $this->crypt->setKey('test');
+        $this->crypt->setKey(random_bytes(Crypt::MIN_SIZE_KEY));
         $plaintext = random_bytes(1024);
 
         $ciphertext = $this->crypt->encrypt($plaintext);
@@ -51,7 +134,7 @@ class CryptTest extends \PHPUnit_Framework_TestCase
      */
     public function testAuthenticationFailure()
     {
-        $this->crypt->setKey('test');
+        $this->crypt->setKey(random_bytes(Crypt::MIN_SIZE_KEY));
         $plaintext = random_bytes(1024);
 
         $ciphertext = $this->crypt->encrypt($plaintext);
